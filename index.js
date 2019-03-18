7bd6217a589faccc2cee4ba2c1042bebdc76197c
mark-marshall <mark.marshallgp@gmail.com>
2019-03-18 13:03:40 +0000

    [MVP] Added get user by id segment

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const db = require('./data/db');
 const server = express();
 
 server.get('/api/users', (req, res) => {
-    db.find()
+    db
+    .find()
     .then(data => {
         res.json(data)
     })
@@ -12,6 +13,24 @@ server.get('/api/users', (req, res) => {
         sendUserError(500, { error: "The users information could not be retrieved." }, res);
         return;
     });
+})
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    db
+    .findById(id)
+    .then(user => {
+        if(user.length === 0){
+            sendUserError(404, { message: "The user with the specified ID does not exist." }, res);
+            return;
+        } {
+            res.json(user);
+        }
+    })
+    .catch(err => {
+        sendUserError(500, { error: "The user information could not be retrieved." }, res);
+        return;
+    })
 })
 
 server.listen(4000, () => console.log('server running on port 4000'));

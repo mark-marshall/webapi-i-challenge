5a70c808d57b9e9bed7d64f8e82de21f03ee7d9d
mark-marshall <mark.marshallgp@gmail.com>
2019-03-18 13:58:37 +0000

    [MVP] Added post user segment

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const db = require('./data/db');
 
 const server = express();
 
+server.use(express.json());
+
 server.get('/api/users', (req, res) => {
   db.find()
     .then(data => {
@@ -48,6 +50,28 @@ server.delete('/api/users/:id', (req, res) => {
     })
     .catch(err => {
       res.status(500).json({ error: 'The user could not be removed' });
+    });
+});
+
+server.post('/api/users', (req, res) => {
+  const user = req.body;
+
+  db.insert(user)
+    .then(data => {
+      if (!user.name || !user.bio) {
+        res
+          .status(400)
+          .json({ errorMessage: 'Please provide name and bio for the user.' });
+      } else {
+        res.status(201).json(data);
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({
+          error: 'There was an error while saving the user to the database',
+        });
     });
 });
 

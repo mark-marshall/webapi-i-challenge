7d0c693b15f41ee9427e084cd1605ee272ddc262
mark-marshall <mark.marshallgp@gmail.com>
2019-03-18 13:42:13 +0000

    [MVP] Added delete user by id segment

--- index.js
+++ index.js
@@ -4,33 +4,51 @@ const db = require('./data/db');
 const server = express();
 
 server.get('/api/users', (req, res) => {
-    db
-    .find()
+  db.find()
     .then(data => {
-        res.json(data)
+      res.status(200).json(data);
     })
     .catch(err => {
-        sendUserError(500, { error: "The users information could not be retrieved." }, res);
-        return;
+      res
+        .status(500)
+        .json({ error: 'The users information could not be retrieved.' });
     });
-})
+});
 
 server.get('/api/users/:id', (req, res) => {
-    const { id } = req.params;
-    db
-    .findById(id)
+  const { id } = req.params;
+  db.findById(id)
     .then(user => {
-        if(user.length === 0){
-            sendUserError(404, { message: "The user with the specified ID does not exist." }, res);
-            return;
-        } {
-            res.json(user);
-        }
+      if (user.id) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The user with the specified ID does not exist.' });
+      }
     })
     .catch(err => {
-        sendUserError(500, { error: "The user information could not be retrieved." }, res);
-        return;
+      res
+        .status(500)
+        .json({ error: 'The user information could not be retrieved.' });
+    });
+});
+
+server.delete('/api/users/:id', (req, res) => {
+  const { id } = req.params;
+  db.remove(id)
+    .then(data => {
+      if (data) {
+        res.status(200).json(data);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The user with the specified ID does not exist.' });
+      }
     })
-})
+    .catch(err => {
+      res.status(500).json({ error: 'The user could not be removed' });
+    });
+});
 
 server.listen(4000, () => console.log('server running on port 4000'));

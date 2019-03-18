2ed78c9270adfaeda59ef76ff006407b6999eac8
mark-marshall <mark.marshallgp@gmail.com>
2019-03-18 12:54:10 +0000

    [MVP] Added get all users segment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// implement your API here
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err => {
+        sendUserError(500, { error: "The users information could not be retrieved." }, res);
+        return;
+    });
+})
+
+server.listen(4000, () => console.log('server running on port 4000'));
